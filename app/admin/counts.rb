2f40a6305298da81b63c6b282067779372c1074f
Troy <troyakins99@yahoo.com>
2021-10-20 21:52:31 -0500

    clean up code

--- app/admin/counts.rb
+++ app/admin/counts.rb
@@ -1,18 +1,3 @@
 ActiveAdmin.register Count do
   permit_params :view
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :view
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:view]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
 end
